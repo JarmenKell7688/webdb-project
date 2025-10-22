6f96aca046b0067f3cb6085db316e04324610492
JarmenKell7688 <villahermosa.jr2017@gmail.com>
2025-10-22 11:30:26 +0800

    Added authorization to Students controller

--- WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d61467ee83aa2d957350b5cb9ce0fac8e50ffd4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5040c5279176b20603149fa24d06a494afa38b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
