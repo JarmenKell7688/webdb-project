bb06c43cc109392c0ac08e810396bb1ce0a235eb
JarmenKell7688 <villahermosa.jr2017@gmail.com>
2025-10-25 17:48:16 +0800

    Checkpoint 25/10/2025

--- WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39dcb75acfe14edc7520d8ffa11377d9afe0edd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+745d011409216001d284355cb13c1716f1e06cb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
